7fe83337dd45f3c1929bdebcfb2fac7af11bcac8
anna3307 <785683307@qq.com>
2022-11-06 11:14:12 +0800

    Initial commit

---
+++ Python_First.py
@@ -0,0 +1,2 @@
+sum = 1 + 2
+print('计算结果为: ' + str(sum))
---
+++ Random_IPv4.py
@@ -0,0 +1,7 @@
+import random
+str1 = random.randint(1,223)
+str2 = random.randint(0,255)
+str3 = random.randint(0,255)
+str4 = random.randint(1,254)
+randomip = str(str1) + '.' + str(str2) + '.' + str(str3) + '.' + str(str4)
+print(randomip)
